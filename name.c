c05b6aa157da9cc665987a64e606c84716709681
Shreyas Lad <shadowtemplates@gmail.com>
2017-02-17 12:18:18 -0800

    Update and rename something.c to name.c

---
+++ name.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+ /* Edit these variables */
+  char * first_name = "John";
+  char * last_name = "Doe";
+ 
+  
+  printf("Your name is " + first_name + " " + last_name);
+}
