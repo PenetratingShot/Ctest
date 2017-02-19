69e6c95e4d0c9ccb78e05bb0ad17da7cbc634221
Shreyas Lad <shadowtemplates@gmail.com>
2017-02-19 08:48:37 -0800

    Update commentrs

--- deathloop.c
+++ deathloop.c
@@ -1,4 +1,12 @@
-/* Running this will create an infinite loop */ 
+/* 
+Ccreated by Shreyas Lad and Talon Bragg 2017
+
+Licensed under the MIT License (see root)
+
+Disclaimer: Running this will create an infinite loop of death. Shreyas and Talon or any affiliates are not reliable for any damage
+or harm that comes to your computer while running this code.
+
+*/ 
 
 #include <stdio.h>
 
