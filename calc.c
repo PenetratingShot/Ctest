233c812def28738fe9ee6103b65d632c40d980f2
Shreyas Lad <shadowtemplates@gmail.com>
2017-03-02 10:49:43 -0800

    Declarations

--- calc.c
+++ calc.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+#include <stdlib.h>
 
+#define Key "Enter the operations you want to do:"
+
+// Function Declarations
+void addition();
+void subtraction();
+void multiplication();
+void division();
+void modulus();
+void power();
+void factorial();
+void calculator_operations();
+
+
+// Start of program
+
+int main() {
+  
+}
