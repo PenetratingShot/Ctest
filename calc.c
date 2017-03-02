554775d1e1ec3e3f1ef26595ac715e9ba0b6df8e
Shreyas Lad <shadowtemplates@gmail.com>
2017-03-02 11:06:07 -0800

    Type key definitions and functions

--- calc.c
+++ calc.c
@@ -19,5 +19,62 @@ void calculator_operations();
 // Start of program
 
 int main() {
+  int X=1;
+  char Clac_oprn;
   
+  //Func callback
+  calculator_operations();
+  
+  while(X) {
+   printf("\n");
+   printf("%s : ", KEY);
+    
+   Calc_oprn=getche();
+    
+   switch(Calc_oprn) {
+     // Case Addition
+     case '+': addition();
+       break;
+       
+     // Case Subtraction  
+     case '-': subtraction();
+       break;
+       
+     // Case Multiplication
+     case '*': multiplication();
+       break;
+       
+     //Case Division
+     case '/': division();
+       break();
+       
+     // Case Modulous
+     case '?': modulus();
+       break();
+       
+     // Case Power
+     case '^': power();
+       break;
+       
+     // Case Factorial
+     case '!': factorial();
+       break;
+       
+     // Func Display Key
+     case 'H':
+     case 'h': calculator_operations();
+       break();
+       
+     // Func Exit  
+     case 'Q':
+     case 'q': exit(0);
+       break;
+       
+     //Func Clear Screen
+     case 'C':
+     case 'c': system("cls");
+       calculator_operations();
+       break;
+   }
+  }
 }
