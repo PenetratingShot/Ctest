7ae5ac9993a83302839ba04949f20aeb52bc57b4
Shreyas Lad <shadowtemplates@gmail.com>
2017-03-06 11:17:06 -0800

    Update test.cc

--- test.cc
+++ test.cc
@@ -1,1 +1,15 @@
+#include <iostream>
 
+using namespace std;
+
+
+/* Signed and unsigned integers */
+
+int main() {
+  short int variable_2; //signed short integer
+  short unsigned int variable _1; //unsigned short integer
+  
+  variable_1 = 5000000;
+  
+  variable_2 = variable_1;
+}
