7b7ce7e7f94371cf2adf43202fe3c7b65c474ac2
Shreyas Lad <shadowtemplates@gmail.com>
2017-03-07 11:09:47 -0800

    Update addition.cc

--- addition.cc
+++ addition.cc
@@ -3,7 +3,9 @@
 using namespace std;
 
 int main() {
+  int a, b, c=0;
   int a = 95893045;
   int b = 456809456;
   int c = a + b;
-  printf("\nThe sum of a and b is &d", c);
+  printf("\nThe sum of a and b is %d", c);
+}
